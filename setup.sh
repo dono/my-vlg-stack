bfd8281e418f234c035389f9e660fac1e9fdb79f
オノ <xononeet@gmail.com>
2022-01-24 01:49:31 +0900

    Mod: refactor comments

--- setup.sh
+++ setup.sh
@@ -3,13 +3,13 @@
 RELEASE_URL="https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/v1.72.0/victoria-metrics-amd64-v1.72.0.tar.gz"
 
 # Move work_dir
-mkdir -p ./work_dir/victoriametrics
-cd ./work_dir/victoriametrics
+mkdir -p ./work_dir/victoriametrics/
+cd ./work_dir/victoriametrics/
 
 # DL
 wget ${RELEASE_URL}
 
-# Copy src to /usr/local/src
+# Copy src to /usr/local/src/
 install -v `basename ${RELEASE_URL}` /usr/local/src/
 
 # Unzip
@@ -18,7 +18,7 @@ tar -xzvf `basename ${RELEASE_URL}`
 # Create user and group to run VictoriaMetrics
 useradd --system --user-group --home /var/lib/victoriametrics --no-create-home --shell /sbin/nologin victoriametrics
 
-# Copy binary to /usr/local/bin
+# Copy binary to /usr/local/bin/
 install -v ./victoria-metrics-prod /usr/local/bin/victoriametrics
 
 # Copy config to /etc/victoriametrics/
